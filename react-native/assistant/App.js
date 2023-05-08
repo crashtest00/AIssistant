485c70511865eac38d45d49a5a5057e1fd041f4d
Jack <jjmarck@gmail.com>
2023-05-07 22:59:51 -0500

    Refactor tts

--- react-native/assistant/App.js
+++ react-native/assistant/App.js
@@ -1,57 +1,16 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { View, StyleSheet, Button, Text } from 'react-native';
-import * as Speech from 'expo-speech';
 import { sayThis } from './speechTools';
 import { getText } from './speechTools';
 import { stopVoiceRecognition } from './speechTools';
 import { startVoiceRecognition } from './speechTools';
 import Voice from '@react-native-voice/voice';
-import { Audio } from 'expo-av'
 
 export default function App() {
-  let [started, setStarted] = useState(false);
-  let [results, setResults] = useState([]);
-
-  useEffect(() => {
-    Voice.onSpeechError = onSpeechError;
-    Voice.onSpeechResults = onSpeechResults;
-
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    }
-  }, []);
-
-  const startSpeechToText = async () => {
-    await Voice.start("en-US");
-    setStarted(true);
-  };
-
-  const stopSpeechToText = async () => {
-    await Voice.stop();
-    setStarted(false);
-  };
-
-  const onSpeechResults = (result) => {
-    setResults(result.value);
-    setThingToSay(result.value[0])
-    console.log(result.value[0]);
-  };
-
-  const onSpeechError = (error) => {
-    console.log(error);
-  };
 
   const [thingToSay, setThingToSay] = useState('No text has been recognized yet');
   voice = 'en-au-x-auc-local'
-
-  const handleVoiceStart = () => {
-    startVoiceRecognition();
-  }
-  const handleVoiceStop = () => {
-    stopVoiceRecognition();
-    console.log("Speech service terminated.")
-  }
 
   return (
     <View style={styles.container}>
@@ -76,33 +35,3 @@ export default function App() {
   }
 })
 
-async function getVoices() {
-  const availableVoices = await Speech.getAvailableVoicesAsync();
-  if (availableVoices.length) {
-    // Everything OK
-  } else {
-    // Try again
-    console.log("No Voices Available. Trying again to get voices...");
-    console.log("Waiting 1000ms");
-    let tryAgainResult = [];
-    for (let x = 0; x <= 10; x++) {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          console.log("Trying Again. Retried ", x + 1, " times.");
-          resolve(null);
-        }, 1000);
-      });
-      const _availableVoices = await Speech.getAvailableVoicesAsync();
-      if (_availableVoices.length) {
-        tryAgainResult = _availableVoices;
-        console.log("Apparently Had Success Trying. Voices: ", tryAgainResult);
-        break;
-      }
-    }
-  }
- if (tryAgainResult.length) { 
-    // Voices OK at tryAgainResult
- } else {
-    throw new Error('Impossible to get Available Voices');
- }
-}
--- react-native/assistant/speechTools.js
+++ react-native/assistant/speechTools.js
@@ -1,8 +1,7 @@
 import * as Speech from 'expo-speech';
 import Voice from '@react-native-voice/voice';
-import { counterEvent } from 'react-native/Libraries/Performance/Systrace';
 
-// Function to speak
+// Function to speak TESTED
 export function sayThis(script, voice) {
   options = {'voice':voice}
   const speak = () => {
@@ -11,6 +10,40 @@ export function sayThis(script, voice) {
 }
 
 // Function to get text from speech
+export const useSpeechToText = () => {
+  let [started, setStarted] = useState(false);
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    }
+  }, []);
+
+  const startSpeechToText = async () => {
+    await Voice.start("en-US");
+    setStarted(true);
+  };
+
+  const stopSpeechToText = async () => {
+    await Voice.stop();
+    setStarted(false);
+  };
+
+  const onSpeechResults = (result) => {
+    setResults(result.value);
+    console.log(result.value[0]);
+  };
+
+  const onSpeechError = (error) => {
+    console.log(error);
+  };
+}
+
+
 export const getText = async () => {
     try {
       await Voice.start('en-US');
@@ -42,6 +75,37 @@ export const startVoiceRecognition = async () => {
   };
   
 // Utility for getting list of voices
+export async function getVoices() {
+  const availableVoices = await Speech.getAvailableVoicesAsync();
+  if (availableVoices.length) {
+    // Everything OK
+  } else {
+    // Try again
+    console.log("No Voices Available. Trying again to get voices...");
+    console.log("Waiting 1000ms");
+    let tryAgainResult = [];
+    for (let x = 0; x <= 10; x++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          console.log("Trying Again. Retried ", x + 1, " times.");
+          resolve(null);
+        }, 1000);
+      });
+      const _availableVoices = await Speech.getAvailableVoicesAsync();
+      if (_availableVoices.length) {
+        tryAgainResult = _availableVoices;
+        console.log("Apparently Had Success Trying. Voices: ", tryAgainResult);
+        break;
+      }
+    }
+  }
+ if (tryAgainResult.length) { 
+    // Voices OK at tryAgainResult
+ } else {
+    throw new Error('Impossible to get Available Voices');
+ }
+}
+
 export async function getVoices() {
   const availableVoices = await Speech.getAvailableVoicesAsync();
   if (availableVoices.length) {
